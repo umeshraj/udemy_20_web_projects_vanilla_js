a212d6388d16c6c9bc832e364be920cfad884f6c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-19 08:26:12 -0400

    Double money

--- 06_array_methods/script.js
+++ 06_array_methods/script.js
@@ -25,6 +25,14 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+// double money
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+
 // add new obj to data array
 function addData(obj) {
   data.push(obj);
@@ -52,3 +60,5 @@ function formatMoney(number) {
 }
 
 // Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
