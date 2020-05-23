bc2d55abec3e46ce0ffa0cece487e80d4029ee46
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-23 09:11:49 -0400

    Responsive design & random meal

--- 09_meal_finder/script.js
+++ 09_meal_finder/script.js
@@ -57,6 +57,19 @@ function getMealById(mealID) {
     });
 }
 
+// Fetch random meal
+function getRandomMeal() {
+  // clear meals and heading
+  mealsEl.innerHTML = "";
+  resultHeading.innerHTML = "";
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+}
+
 // add meal to DOM
 function addMealToDOM(meal) {
   const ingredients = [];
@@ -90,6 +103,8 @@ function addMealToDOM(meal) {
 
 // Event listeners
 submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
+
 mealsEl.addEventListener("click", (e) => {
   const mealInfo = e.path.find((item) => {
     if (item.classList) {
