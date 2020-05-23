f82eb80bd38917f7b1ccd11831cac889c10a6d5b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-23 07:57:34 -0400

    Show meals from fetch api

--- 09_meal_finder/script.js
+++ 09_meal_finder/script.js
@@ -17,6 +17,30 @@ function searchMeal(e) {
 
   // check for empty
   if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for ${term}:</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Try again.</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+          <div class="meal"> 
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+          <div class="meal-info" data-mealID="${meal.idMeal}"> 
+          <h3> ${meal.strMeal}</h3>
+          </div>
+          </div>`
+            )
+            .join("");
+        }
+      });
+    // clear search text
+    search.value = "";
   } else {
     alert("Please enter a search term");
   }
