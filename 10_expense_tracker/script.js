60abd679e35723cf316d357667970911e4ab8db8
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-25 07:47:46 -0400

    Delete button

--- 10_expense_tracker/script.js
+++ 10_expense_tracker/script.js
@@ -53,7 +53,9 @@ function addTransactionDOM(transaction) {
 
   item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
-    <button class="delete-btn">x</button>
+    <button class="delete-btn" onclick="removeTransaction(${
+      transaction.id
+    })">x</button>
   `;
   list.appendChild(item);
 }
@@ -77,6 +79,12 @@ function updateValues() {
   balance.innerText = `$${total}`;
   money_plus.innerText = `$${income}`;
   money_minus.innerText = `$${expense}`;
+}
+
+// remove transaction
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+  init();
 }
 
 // init app
