4528ed4680b8aae62d2f6e7bae796560906023ef
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-05-28 08:55:58 -0400

    Fix bug in number of posts being shown

--- 12_infinite_scroll/script.js
+++ 12_infinite_scroll/script.js
@@ -3,14 +3,14 @@ const loading = document.querySelector(".loader");
 const filter = document.getElementById("filter");
 
 let limit = 3;
-let page = 1;
+let page = 0;
 
 // Fetch posts from API
 async function getPosts() {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
 
   const data = await res.json();
-  return data.slice(0, limit);
+  return data.slice(page * limit, page * limit + limit);
 }
 
 // show posts in DOM
@@ -36,11 +36,11 @@ function showLoading() {
   loading.classList.add("show");
   setTimeout(() => {
     loading.classList.remove("show");
-    setTimeout(() => {
-      limit += 3;
-      showPosts();
-    }, 300);
   }, 1000);
+  setTimeout(() => {
+    page++;
+    showPosts();
+  }, 300);
 }
 
 // filter posts by input
@@ -66,7 +66,7 @@ showPosts();
 window.addEventListener("scroll", () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-  if (scrollTop + clientHeight >= scrollHeight - 5) {
+  if (scrollTop + clientHeight >= scrollHeight - 0) {
     showLoading();
   }
 });
