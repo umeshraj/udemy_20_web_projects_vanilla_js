f543be0033cdb5fc5aac5880c414a47a306a8a57
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-02 08:49:11 -0400

    Generate ul with map

--- 16_lyrics_search/script.js
+++ 16_lyrics_search/script.js
@@ -17,19 +17,36 @@ async function searchSongs(term) {
 
 // show song and artist in DOM
 function showData(data) {
-  let output = "";
-  data.data.forEach((song) => {
-    output += `
-      <li>
-      <span>
-      <strong>${song.artist.name}</strong> - ${song.title}
-      </span> 
-      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}"> Get Lyrics </button>
-      </li>
-      `;
-  });
+  //   let output = "";
+  //   data.data.forEach((song) => {
+  //     output += `
+  //       <li>
+  //       <span>
+  //       <strong>${song.artist.name}</strong> - ${song.title}
+  //       </span>
+  //       <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}"> Get Lyrics </button>
+  //       </li>
+  //       `;
+  //   });
+  //   result.innerHTML = `
+  //   <ul class="songs">${output}</ul>
+  //   `;
+
   result.innerHTML = `
-  <ul class="songs">${output}</ul>
+  <ul class="songs">
+  ${data.data
+    .map(
+      (song) => `
+  <li>
+  <span>
+  <strong>${song.artist.name}</strong> - ${song.title}
+  </span> 
+  <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}"> Get Lyrics </button>
+  </li>
+  `
+    )
+    .join("")}
+  </ul>
   `;
 }
 
