f945dd3ea899c5a18f0e4fcab12198bc70275928
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-02 08:58:36 -0400

    Adding prev/next

--- 16_lyrics_search/script.js
+++ 16_lyrics_search/script.js
@@ -17,21 +17,6 @@ async function searchSongs(term) {
 
 // show song and artist in DOM
 function showData(data) {
-  //   let output = "";
-  //   data.data.forEach((song) => {
-  //     output += `
-  //       <li>
-  //       <span>
-  //       <strong>${song.artist.name}</strong> - ${song.title}
-  //       </span>
-  //       <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}"> Get Lyrics </button>
-  //       </li>
-  //       `;
-  //   });
-  //   result.innerHTML = `
-  //   <ul class="songs">${output}</ul>
-  //   `;
-
   result.innerHTML = `
   <ul class="songs">
   ${data.data
@@ -48,6 +33,31 @@ function showData(data) {
     .join("")}
   </ul>
   `;
+
+  // next and previous
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ""
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ""
+      }
+    `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+// get prev/next results
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+  showData(data);
 }
 
 // Event listeners
