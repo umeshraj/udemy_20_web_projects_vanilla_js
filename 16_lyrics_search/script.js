fe95880780663b88c22c08a4a912545ee57a0a3d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-02 08:46:50 -0400

    Using for each to make the ul

--- 16_lyrics_search/script.js
+++ 16_lyrics_search/script.js
@@ -16,7 +16,22 @@ async function searchSongs(term) {
 }
 
 // show song and artist in DOM
-function showData(data) {}
+function showData(data) {
+  let output = "";
+  data.data.forEach((song) => {
+    output += `
+      <li>
+      <span>
+      <strong>${song.artist.name}</strong> - ${song.title}
+      </span> 
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}"> Get Lyrics </button>
+      </li>
+      `;
+  });
+  result.innerHTML = `
+  <ul class="songs">${output}</ul>
+  `;
+}
 
 // Event listeners
 form.addEventListener("submit", (e) => {
