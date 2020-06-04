a8bebc6e1b45766c8b257fa66c2cddd01c81438e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-04 08:42:55 -0400

    Move paddle

--- 18_breakout_game/script.js
+++ 18_breakout_game/script.js
@@ -90,15 +90,63 @@ function drawBricks() {
   });
 }
 
+// move paddle on canvas
+function movePaddle() {
+  paddle.x += paddle.dx;
+
+  // wall detection
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
+
 // draw everything
 function draw() {
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
   drawScore();
   drawBricks();
 }
 
-draw();
+// update canvas drawing and animation
+function update() {
+  movePaddle();
+  // draw everything
+  draw();
+
+  requestAnimationFrame(update);
+}
+
+update();
+
+// keydown event
+function keyDown(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    paddle.dx = paddle.speed;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+function keyUp(e) {
+  if (
+    e.key === "Right" ||
+    e.key === "ArrowRight" ||
+    e.key === "Left" ||
+    e.key === "ArrowLeft"
+  ) {
+    paddle.dx = 0;
+  }
+}
+
+// keyboard event handlers
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
 // Rules and close event handlers
 rulesBtn.addEventListener("click", () => {
