c02df35c58a0a1ef9544f4501cf124dd955f706b
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-04 08:32:09 -0400

    Drawing brick wall

--- 18_breakout_game/script.js
+++ 18_breakout_game/script.js
@@ -5,6 +5,8 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let score = 0;
+const brickRowCount = 9;
+const brickColumnCount = 5;
 
 // create ball properties
 const ball = {
@@ -26,6 +28,31 @@ const paddle = {
   dx: 0,
 };
 
+// create brick properties
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+// create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = {
+      x,
+      y,
+      ...brickInfo,
+    };
+  }
+}
+
 // draw ball on canvas
 function drawBall() {
   ctx.beginPath();
@@ -44,17 +71,31 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+// draw score on canvas
+function drawScore() {
+  ctx.font = "20px Arial";
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+// draw bricks on canvas
+function drawBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "#0095dd" : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
+
 // draw everything
 function draw() {
   drawBall();
   drawPaddle();
   drawScore();
-}
-
-// draw score on canvas
-function drawScore() {
-  ctx.font = "20px Arial";
-  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+  drawBricks();
 }
 
 draw();
