0eb0cd9f6675bf4df2394dac0e204931513f8d73
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-06 07:43:36 -0400

    Change order of list

--- 20_drag_drop_list/script.js
+++ 20_drag_drop_list/script.js
@@ -22,18 +22,23 @@ createList();
 
 // insert list items into DOM
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("data-index", index);
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      //   console.log(person);
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
 
-    listItem.innerHTML = `
+      listItem.innerHTML = `
     <span class="number"> ${index + 1}</span>
     <div class="draggable" draggable="true">
         <p class="person-name">${person}</p>
         <i class="fas fa-grip-lines"></i>
     </div>
     `;
-    listItems.push(listItem);
-    draggable_list.appendChild(listItem);
-  });
+      listItems.push(listItem);
+      draggable_list.appendChild(listItem);
+    });
 }
