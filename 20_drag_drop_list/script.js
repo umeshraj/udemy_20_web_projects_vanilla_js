93ee35707bc807eb170fd36ddb574ebae35570f9
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-07 09:14:17 -0400

    Move items with drag drop

--- 20_drag_drop_list/script.js
+++ 20_drag_drop_list/script.js
@@ -41,4 +41,56 @@ function createList() {
       listItems.push(listItem);
       draggable_list.appendChild(listItem);
     });
+
+  addEventListeners();
+}
+
+function dragStart() {
+  // console.log("Event: ", "dragStart");
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+}
+
+function dragEnter() {
+  // console.log("Event: ", "dragEnter");
+  this.classList.add("over");
+}
+
+function dragLeave() {
+  // console.log("Event: ", "dragLeave");
+  this.classList.remove("over");
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  // console.log("Event: ", "dragOver");
+}
+
+function dragDrop() {
+  // console.log("Event: ", "drop");
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove("over");
+}
+
+function swapItems(fromIndex, toIndex) {
+  const item1 = listItems[fromIndex].querySelector(".draggable");
+  const item2 = listItems[toIndex].querySelector(".draggable");
+  listItems[fromIndex].appendChild(item2);
+  listItems[toIndex].appendChild(item1);
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
 }
