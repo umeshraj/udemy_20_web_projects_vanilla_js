eae7dda449016e90d8c1d209f198c2c0c7d56328
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-07 09:21:13 -0400

    Check order of sorted people

--- 20_drag_drop_list/script.js
+++ 20_drag_drop_list/script.js
@@ -72,11 +72,25 @@ function dragDrop() {
   this.classList.remove("over");
 }
 
+// swap list items
 function swapItems(fromIndex, toIndex) {
   const item1 = listItems[fromIndex].querySelector(".draggable");
   const item2 = listItems[toIndex].querySelector(".draggable");
   listItems[fromIndex].appendChild(item2);
   listItems[toIndex].appendChild(item1);
+}
+
+// check the order of list items
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
+  });
 }
 
 function addEventListeners() {
@@ -94,3 +108,5 @@ function addEventListeners() {
     item.addEventListener("dragleave", dragLeave);
   });
 }
+
+check.addEventListener("click", checkOrder);
